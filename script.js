6422e64bc9c8e74c1180e88b8e4943aa1ac5e125
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-22 10:06:14 -0300

    dia 3 concluido

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('form');
+const nlwSetup = new NLWSetup(form);
 
+const data = {
+  run: ['01-01', '01-02', '01-06', '01-07',
+ '01-08'],
+  takepills: ['01-03'],
+  journal: ['01-02'],
+};
+
+nlwSetup.setData(data);
+nlwSetup.load();
